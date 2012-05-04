4ad0c3d24749e2c4eed6963c13873d6bba4c429d
Mario Onekanda <mario.onekanda@hotmail.com>
2012-05-04 22:15:14 +0200

    modified css & database

--- application/views/header.php
+++ application/views/header.php
@@ -6,19 +6,21 @@
 	<script type="text/javascript" src="./static/myscript.js"></script>
 </head>
 <body>
-	<div id="wrapper">
-		
-		<div id="header-content">
+	
+	<div id="top">
 			<div id="logo">
 				<a href="#"><img src="./static/img/header/logo.png" /><span class="logo-title">By  Mario Onekanda</span></a>
 			</div>
 			<div id="loggin-form">
 				<form action="#" action="post">
-					<input type="text" name="username" class="user-field" /><p></p>
+					<input type="text" name="username" class="user-field" value="user"/>
 				
 					<input type="password" name="password" class="pass-field" /><input type="checkbox" name="session" value="remember"  class="remember-checkbox"/>
-					<label class="label1">Forget your password?</label>
+					<label class="label1">Remember me</label><label class="label2">Forget your password?</label><label class="label3">Wanna be a member?</label>
 					<input type="submit" name="submit" value="Login" class="submit-btn" />
 				</form>
 			</div>
-		</div>
+	</div>
+	<div id="wrapper">
+		
+		
---
+++ db/onekweb-project.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Värd: localhost
+-- Skapad: 04 maj 2012 kl 20:09
+-- Serverversion: 5.5.16
+-- PHP-version: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Databas: `onekweb-project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `pages`
+--
+
+CREATE TABLE IF NOT EXISTS `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject_id` int(11) NOT NULL,
+  `menu_name` varchar(30) NOT NULL,
+  `position` int(3) NOT NULL,
+  `visible` tinyint(1) NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `subjects`
+--
+
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_name` varchar(30) NOT NULL,
+  `position` int(13) NOT NULL,
+  `visible` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `hashed_password` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
